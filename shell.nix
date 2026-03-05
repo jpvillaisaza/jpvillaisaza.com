6c21e8a740e7f7d39688195a81bcdee315c57368
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2026-03-05 18:06:52 -0500

    Use channel instead

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 let
-  # nixpkgs-25.11-darwin
-  commit = "fabb8c9deee281e50b1065002c9828f2cf7b2239";
-  url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
+  channel = "nixos-25.11";
+  url = "https://github.com/NixOS/nixpkgs/archive/${channel}.tar.gz";
   pkgs = import (fetchTarball url) { };
 in
 
